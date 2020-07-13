e7fadad11a73257de318f1b66c2da259d008390e
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-13 16:14:05 -0400

    Finished Queries Basic

--- queries.sql
+++ queries.sql
@@ -17,8 +17,13 @@ LIMIT 10
 
 -- Find all customers that live in London, Madrid, or Brazil
 
+SELECT * FROM Customers
+WHERE City = 'London' or Country = 'Brazil' or City = 'Madrid'
+
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
+INSERT INTO Customers
+VALUES(92, 'The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', 111, 'Middle Earth')
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
 
