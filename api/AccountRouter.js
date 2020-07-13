63b48e0f000bd10dee33c50e229b4291adf96802
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-13 16:47:37 -0400

    Account Routes Done

--- api/AccountRouter.js
+++ api/AccountRouter.js
@@ -43,6 +43,46 @@ router.post("/", (req, res) => {
     });
 });
 
+router.put("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then((count) => {
+      if (count > 0) {
+        db.select("*")
+          .from("accounts")
+          .where({ id: req.params.id })
+          .then((account) => {
+            res.status(200).json({ data: account[0] });
+          })
+          .catch((err) => {
+            handleError(err, res);
+          });
+      } else {
+        res.status(404).json({ message: "there were no records to update" });
+      }
+    })
+    .catch((err) => {
+      handleError(err, res);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .del()
+    .then((count) => {
+      if (count > 0) {
+        res.status(200).json({ data: count });
+      } else {
+        res.status(404).json({ message: "there were no records to delete" });
+      }
+    })
+    .catch((err) => {
+      handleError(err, res);
+    });
+});
+
 function handleErrors(err, res) {
   res.status(500).json({ error: err });
 }
